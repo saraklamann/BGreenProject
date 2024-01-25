b44f3bb63bf2355e3847e5a3b493d90368850197
Sara Caroline Klamann Jucoski <119754784+saraklamann@users.noreply.github.com>
2024-01-25 15:19:24 -0300

    Delete B Green - Project - SA parcial directory

--- B Green - Project - SA parcial/Evento.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Evento {
-    public String nome;
-    public String data;
-    public String endereco;
-    public String organizador;
-}
--- B Green - Project - SA parcial/Postos.java
+++
@@ -1,6 +0,0 @@
-public class Postos {
-    public String rua;
-    public String numero;
-    public String cidade;
-    public Empresa empresa;
-}
